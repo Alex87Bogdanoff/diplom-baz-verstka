3618cd23bd563dd00dcc741e24b2d252b2a6a879
Alex87Bogdanoff <alex87bogdanoff@gmail.com>
2021-05-15 00:03:31 +0300

    Section_Catalog_1920.5

--- js/index.js
+++ js/index.js
@@ -1,5 +1,34 @@
+/* Селект Header */
+const element = document.querySelector('select');
+const choices = new Choices(element, {
+  searchEnabled: false
+});
+
+/* Клик Header */
+document.querySelector('.list-item__btn-1').addEventListener('click', function () {
+  document.querySelector('.list-dropdown-1').classList.toggle('is-active')
+})
+
+document.querySelector('.list-item__btn-2').addEventListener('click', function () {
+  document.querySelector('.list-dropdown-2').classList.toggle('is-active')
+})
+
+document.querySelector('.list-item__btn-3').addEventListener('click', function () {
+  document.querySelector('.list-dropdown-3').classList.toggle('is-active')
+})
+
+document.querySelector('.list-item__btn-4').addEventListener('click', function () {
+  document.querySelector('.list-dropdown-4').classList.toggle('is-active')
+})
+
+document.querySelector('.list-item__btn-5').addEventListener('click', function () {
+  document.querySelector('.list-dropdown-5').classList.toggle('is-active')
+})
+
+
+
 /* Слайдер Gallery */
-const swiperGallery = new Swiper ('.gallery__swiper', {
+const swiperGallery = new Swiper('.gallery__swiper', {
   speed: 600,
   spaceBetween: 50,
   slidesPerView: 3,
@@ -24,28 +53,35 @@ const swiperGallery = new Swiper ('.gallery__swiper', {
   },
 
   breakpoints: {
-    1: {
+    320: {
       spaceBetween: 0,
       slidesPerView: 1,
       slidesPerGroup: 1,
       slidesPerColumn: 1,
     },
 
-    321: {
+    460: {
       spaceBetween: 34,
       slidesPerView: 2,
       slidesPerGroup: 2,
-      slidesPerColumn: 2,
+      slidesPerColumn: 1,
     },
-    
-    769: {
+
+    768: {
       spaceBetween: 34,
       slidesPerView: 2,
       slidesPerGroup: 2,
       slidesPerColumn: 2,
     },
 
-    1801: {
+    1024: {
+      spaceBetween: 50,
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+      slidesPerColumn: 2,
+    },
+
+    1920: {
       spaceBetween: 50,
       slidesPerView: 3,
       slidesPerGroup: 3,
@@ -55,11 +91,13 @@ const swiperGallery = new Swiper ('.gallery__swiper', {
 });
 
 /* Клик Gallery */
+$('#gallery__hover').attr('open-info');
+
 $(document).ready(function () {
-  
+
   $(".slide-hover").click(function () {
     $('#gallery__hover').fadeIn(300);
-    var iddiv = $(this).attr("id-hover");
+    var iddiv = $(this).attr("data-path");
     $('#' + iddiv).fadeIn(300);
     $('#gallery__hover').attr('open-info', iddiv);
     return false;
@@ -112,10 +150,3 @@ document.addEventListener('DOMContentLoaded', function () {
     })
   })
 })
-
-const element = document.querySelector('select');
-const choices = new Choices(element, {
-  searchEnabled: false
-});
-
-
